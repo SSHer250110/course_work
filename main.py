40457026f618f40b0cdbaa2d9fd2c0b0abfd383a
Serg_Shershikov <ssher250110@yandex.ru>
2023-11-28 00:47:19 +0300

    added part of the code func main

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    pass
+from settings import OPERATIONS_PATH
+from src.utils import load_data, get_instances_class, get_executed_operations
 
+
+def main():
+    operations = load_data(OPERATIONS_PATH)
+    instances = get_instances_class(operations)
+    executed_operations = get_executed_operations(instances)
 
 if __name__ == '__main__':
     main()
