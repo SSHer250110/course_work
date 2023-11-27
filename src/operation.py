1af8c6e957fa18542e3df4be36e0740e4ba0cade
Serg_Shershikov <ssher250110@yandex.ru>
2023-11-27 23:02:54 +0300

    func convert date

--- src/operation.py
+++ src/operation.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 class Operation:
     def __init__(
             self,
@@ -10,9 +13,15 @@ class Operation:
             to: str
     ):
         self.pk = pk
-        self.date = date
+        self.date = self.convert_date(date)
         self.state = state
         self.operation_amount = operation_amount
         self.description = description
         self.from_ = from_
         self.to = to
+
+    def convert_date(self, date: str):
+        format_data = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+        return format_data.strftime("%d.%m.%Y")
+        # iso_date = datetime.fromisoformat(date)
+        # return iso_date.strftime("%d.%m.%Y")
