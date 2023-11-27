a021e50bb8e3b9b0fc1ee838e3a1e8fa0944a425
Serg_Shershikov <ssher250110@yandex.ru>
2023-11-28 00:45:50 +0300

    func convert info payment and editing func convert date

--- src/operation.py
+++ src/operation.py
@@ -17,11 +17,25 @@ class Operation:
         self.state = state
         self.operation_amount = operation_amount
         self.description = description
-        self.from_ = from_
-        self.to = to
+        self.from_ = self.convert_info_payment(from_)
+        self.to = self.convert_info_payment(to)
 
-    def convert_date(self, date: str):
+    def convert_date(self, date: str) -> datetime.date:
         format_data = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
         return format_data.strftime("%d.%m.%Y")
         # iso_date = datetime.fromisoformat(date)
         # return iso_date.strftime("%d.%m.%Y")
+
+    def convert_info_payment(self, info_payment: str) -> str:
+        if info_payment:
+            info_list = info_payment.split()
+            if info_payment.startswith("Счет"):
+                num_payment = info_list.pop()
+                num_payment = f"**{num_payment[-4:]}"
+                info_list.append(num_payment)
+            else:
+                num_payment = info_list.pop()
+                num_payment = f"{num_payment[:4]} {num_payment[5:7]}** **** {num_payment[-4:]}"
+                info_list.append(num_payment)
+            return " ".join(info_list)
+        return ""
