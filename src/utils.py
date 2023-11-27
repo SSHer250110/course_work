21cabd20544058a3bc6583a155f6172a3776e2b4
Serg_Shershikov <ssher250110@yandex.ru>
2023-11-27 21:21:54 +0300

    func instances class

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,25 @@
 import json
 
+from src.operation import Operation
+
 
 def load_data(path: str) -> list[dict]:
     # Чтение данных из файла
     with open(path, "r", encoding="utf-8") as file:
         return json.load(file)
+
+
+def get_instances_class(operations: list[dict]) -> list[Operation]:
+    operation_instances = []
+    for operation in operations:
+        if operation:
+            operation_instances.append(Operation(
+                pk=operation["id"],
+                state=operation["state"],
+                date=operation["date"],
+                operation_amount=operation["operation_amount"],
+                description=operation["description"],
+                from_=operation["from"],
+                to=operation["to"]
+            ))
+    return operation_instances
