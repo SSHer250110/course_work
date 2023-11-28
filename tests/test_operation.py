3e8f40af6750053008a88265e19380238426013b
Serg_Shershikov <ssher250110@yandex.ru>
2023-11-28 18:49:33 +0300

    added docstrings

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -2,6 +2,9 @@ from src.operation import Operation
 
 
 def test_operation_convert_date():
+    """
+    Тест для проверки метода класса преобразования даты в нужный вид
+    """
     operation = Operation(pk=123421,
                           state="EXECUTED",
                           date="2019-07-03T18:35:29.512364",
@@ -20,6 +23,9 @@ def test_operation_convert_date():
 
 
 def test_operation_convert_info_payment():
+    """
+    Тест для проверки метода вывода информации о платеже в нужном виде
+    """
     operation = Operation(pk=123421,
                           state="EXECUTED",
                           date="2019-07-03T18:35:29.512364",
